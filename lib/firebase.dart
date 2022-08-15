d3270c31cc2e657fab1c46fe2eafd53a38dd978b
Sifat-16 <sifatalamcep16@gmail.com>
2022-08-15 17:01:36 +0600

    first commit

---
+++ lib/firebase.dart
@@ -0,0 +1,57 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+import 'package:icbadmin/rechargemodel.dart';
+
+import 'WithdrawModel.dart';
+
+class FireBase{
+
+  FirebaseAuth auth = FirebaseAuth.instance;
+  FirebaseFirestore store = FirebaseFirestore.instance;
+
+  signIn()async{
+    await auth.signInWithEmailAndPassword(email: "admin@gmail.com", password: "123456");
+  }
+
+    Stream<List<WithdrawModel>> allwithdraws(){
+      return store.collection("withdraws").snapshots().map((event) => event.docs.map((e) => WithdrawModel.fromJson(e.data())).toList());
+    }
+
+    allowWithdraw(String uid)async{
+      final x = store.collection("withdraws").where("uid", isEqualTo: uid).where("granted", isEqualTo: false).get().then((value) async{
+        final y = store.collection("withdraws").doc(value.docs.first.id);
+        await y.update({
+          "granted":true
+        });
+
+
+      });
+
+      /*print(x.docs.first.data());
+
+      x.docs.first.data().update("granted", (value) => true);
+      print(x.docs.first.data()["granted"]);*/
+
+    }
+
+    Stream<List<RechargeModel>> allrechargerequest(){
+        return store.collection("recharges").snapshots().map((event) => event.docs.map((e) => RechargeModel.fromJson(e.data())).toList());
+    }
+
+    acceptRecharge(RechargeModel rechargeModel)async{
+    String doc = "";
+    final k = await  store.collection("recharges").where("uid", isEqualTo: rechargeModel.uid).get().then((value) {
+      doc = value.docs.first.id;
+    });
+    final s = await store.collection("userProfile").doc(rechargeModel.uid).get();
+      final x = store.collection("userProfile").doc(rechargeModel.uid).update(
+          {
+            "balance":s.data()!["balance"]+double.parse(rechargeModel.amount!)
+          }).then((value) async{
+            final y = await store.collection("recharges").doc(doc).delete();
+      });
+    }
+
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,129 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:icbadmin/WithdrawModel.dart';
+import 'package:icbadmin/firebase.dart';
+import 'package:icbadmin/rechargemodel.dart';
+
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  FireBase fireBase = FireBase();
+  FirebaseAuth auth = FirebaseAuth.instance;
+  bool withdraw = true;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    fireBase.signIn();
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.limeAccent,
+        title: Text("admin"),
+        actions: [
+          TextButton(onPressed: (){
+            this.setState(() {
+              withdraw=!withdraw;
+            });
+          }, child: Text(withdraw?"withdraw":"recharge"))
+        ],
+      ),
+      body: StreamBuilder(
+          stream: auth.authStateChanges(),
+          builder: (context, snapshot) {
+            if(snapshot.connectionState==ConnectionState.active){
+              if(snapshot.hasData){
+                return withdraw?StreamBuilder<List<WithdrawModel>>(
+                  stream: fireBase.allwithdraws(),
+                    builder: (context, s){
+                      return s.hasData?ListView.builder(
+                        itemCount: s.data?.length,
+                          itemBuilder: (context, index){
+                            return ListTile(
+                              title: Text("${s.data![index].number}"),
+                              subtitle: Text("${s.data![index].amount}"),
+                              trailing: Expanded(
+
+                                child: !s.data![index].granted? IconButton(onPressed: ()async{
+
+                                  await fireBase.allowWithdraw(s.data![index].uid!);
+
+                                }, icon: Icon(Icons.add)):SizedBox.shrink()
+                              ),
+                            );
+                          }
+                      ):CircularProgressIndicator();
+                    }
+                ):StreamBuilder<List<RechargeModel>>(
+                    stream: fireBase.allrechargerequest(),
+                    builder: (context, s){
+                      return s.hasData?ListView.builder(
+                          itemCount: s.data?.length,
+                          itemBuilder: (context, index){
+                            return ListTile(
+                              title: Text("${s.data![index].transactionId}"),
+                              subtitle: Text("${s.data![index].amount}"),
+                              trailing: Expanded(
+
+                                  child: IconButton(onPressed: ()async{
+
+                                    await fireBase.acceptRecharge(s.data![index]);
+
+                                  }, icon: Icon(Icons.add))
+                              ),
+                            );
+                          }
+                      ):CircularProgressIndicator();
+                    }
+                );
+              }
+            }
+            if(snapshot.connectionState==ConnectionState.waiting){
+              return Scaffold(body: Center(child: CircularProgressIndicator(),),);
+            }
+            return Center(child: Text("Error"),);
+          }
+      ),
+    );
+  }
+}
+
+
+
+
